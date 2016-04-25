a234399998817f82a5b5a4fd39677531ff1e86c7
Yuta K <ugwis@icloud.com>
2016-04-25 20:26:11 +0900

    httpsへリダイレクト

--- login.js
+++ login.js
@@ -1,8 +1,4 @@
-
-
-function url_parse(){
-	var url = location.href;
-	var query = url.split("?")[1];
+function parse_query(query){
 	if(query === undefined) return;
 	var queryies = query.split("&");
 	var ret = {};
@@ -13,13 +9,28 @@ function url_parse(){
 	return ret;
 }
 
-function login(){
-	var query = url_parse();
-	if(query.id === undefined || query.pass === undefined) return;
-	var user = query.id;
-	var passwd = query.pass;
+function parse_url(){
+	var url = location.href;
+	var parsed = url.match(/(\w*):\/\/([\w|\.|-]*)\/(.*)/);
+	return {
+		method:parsed[1],
+		domain:parsed[2],
+		directory:parsed[3].split("?")[0],
+		query:parsed[3].split("?")[1]
+	}
+}
+
+function login(user,passwd){
+	if(user === undefined || passwd === undefined) return;
 	var login_img = document.getElementById("login_img");
-	console.log(user);
-	console.log(passwd);
 	login_img.src = "https://acsu.shinshu-u.ac.jp/ActiveCampus/module/Login.php?mode=Login&clickcheck=0&login="+user+"&passwd="+passwd;
+}
+
+window.onload = function(){
+	var parsed_url = parse_url();
+	if(parsed_url.method === "http"){
+		location.replace("https://" + parsed_url.domain + "/" + parsed_url.directory + (parsed_url.query !== undefined?"?" + parsed_url.query:""));
+	}
+	var query = parse_query(parsed_url.query);
+	login(query.id, query.passwd);
 }
